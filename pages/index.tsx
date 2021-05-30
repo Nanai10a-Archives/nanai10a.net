cfe563595a902fc4e748cad5c82f809ff6027288
Nanai10a <Nanai10a@outlook.jp>
2021-05-30 18:47:11 +0900

    ✨ 取り敢えず完成.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage, GetStaticProps } from "next";
 import AppContainer from "../component/AppContainer";
 import RenderMarkdown, { Props as RenderMarkdownProps } from "../component/RenderMarkdown";
+import fetch from "node-fetch";
+import Header from "../component/Header";
 
 import styles from "../styles/index.module.scss";
-import Header from "../component/Header";
 
 const Index: NextPage<Props> = (props: Props) => {
   return (
@@ -30,13 +31,18 @@ type Props = {
 
 // noinspection JSUnusedGlobalSymbols
 export const getStaticProps: GetStaticProps<Props> = async () => {
+  const res = await fetch(process.env["API_URL"] ?? "");
+  const obj = JSON.parse(await res.text());
+
+  const props: Array<{ markdown: string }> = [];
+
+  obj.descriptions.descriptions.map(({ markdown }: { markdown: string }) =>
+    props.push({ markdown }),
+  );
+
   return {
     props: {
-      descriptionsProps: [
-        {
-          markdown: "",
-        },
-      ],
+      descriptionsProps: props,
     },
   };
 };
